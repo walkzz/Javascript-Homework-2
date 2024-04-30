ed04e249c7c828bc45b10471a57197b9eb0bc237
Pavel Toshinski <pavelce2000@hotmail.com>
2024-04-30 19:49:21 +0200

    Implemented switch case and template literals

--- exercies-2.js
+++ exercies-2.js
@@ -41,4 +41,29 @@ let score = 78;
 checkScore(score); // Grade: C
 
 // b)
-let dayNumber = Math.floor(Math.random() * 8);
+let dayNumber = Math.floor(Math.random() * 10);
+switch (dayNumber) {
+    case 1:
+        console.log(`The day is Sunday.`);
+        break;
+    case 2:
+        console.log(`The day is Monday.`);
+        break;
+    case 3:
+        console.log(`The day is Tuesday.`);
+        break;
+    case 4:
+        console.log(`The day is Wednesday..`);
+        break;
+    case 5:
+        console.log(`The day is Thursday.`);
+        break;
+    case 6:
+        console.log(`The day is Friday.`);
+        break;
+    case 7:
+        console.log(`The day is Saturday.`);
+        break;
+    default:
+        console.log(`Invalid day number. Please enter a number between 1 and 7.`);
+}
